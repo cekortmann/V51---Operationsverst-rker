6d34c94c6991214ee979174c50500be8be24276e
Jan Lucca Viola <janlucca.viola@tu-dortmund.de>
2024-04-17 19:26:42 +0200

    :(

--- genvar.py
+++ genvar.py
@@ -23,43 +23,14 @@ t, U = np.genfromtxt('genvarAmp.txt', unpack=True, skip_header=1)
 
 # Ausgleichsrechung nach Gaußverteilung
 
-<<<<<<< HEAD
-def f(U0,n, x):
-    return U0*np.exp(n*x/(20*10000*22))  # b = 2*sigma**2
-||||||| 3984183
-def g(U0,n, x):
-    return U0*np.exp(n*x/(20*10000*22))  # b = 2*sigma**2
-=======
 def g(U0,T, x):
     return U0*np.exp(x*10**(-9)/T)  # b = 2*sigma**2
->>>>>>> f4770250402e053435f95e10ddf7e899dc48b11a
 
-<<<<<<< HEAD
-para, pcov = curve_fit(f,t,U)
-n, U0 = para
-||||||| 3984183
-para, pcov = curve_fit(g,t,U)
-U0, n = para
-=======
 para, pcov = curve_fit(g,t,U)
 U0, T = para
->>>>>>> f4770250402e053435f95e10ddf7e899dc48b11a
 pcov = np.sqrt(np.diag(pcov))
-<<<<<<< HEAD
-fn, fU = pcov
-||||||| 3984183
-fU, fn = pcov
-=======
 fU, fT = pcov
->>>>>>> f4770250402e053435f95e10ddf7e899dc48b11a
 uU = ufloat(U0, fU) 
-uT = ufloat(T, fT)
-
-print('uU:',uU,'un:',uT)
-
-xx = np.linspace(0, 800, 10**4)
-
-plt.plot(t, U, 'xr', markersize=6 , label = 'Messdaten', alpha=0.5)
 plt.plot(xx, f(xx, *para), '-b', linewidth = 1, label = 'Ausgleichsfunktion', alpha=0.5)
 plt.xlabel(r'$t \, / \, \mathrm{ns}$')
 plt.ylabel(r'$U_A \, / \, \mathrm{V}$')
