449d4f7fa2033d589c54d626c6660829cea6fe02
Jan Lucca Viola <janlucca.viola@tu-dortmund.de>
2024-05-03 19:31:23 +0200

    Theorie ergänzung

--- genvar.py
+++ genvar.py
@@ -15,7 +15,7 @@ from uncertainties.unumpy import (nominal_values as noms,   # Wert:             
 # Plot 1:
 
 t, U = np.genfromtxt('genvarAmp.txt', unpack=True, skip_header=1) 
-
+t1,U1= np.genfromtxt('genvarAmpFreestyle.txt', unpack=True, skip_header=1)
 # für den initial guess bei curvefit()
 #n = len(f)                             # Anzahl der Daten
 #mean = sum(r*V)/n                      # Mittelwert
@@ -23,23 +23,27 @@ t, U = np.genfromtxt('genvarAmp.txt', unpack=True, skip_header=1)
 
 # Ausgleichsrechung nach Gaußverteilung
 
-def g(x,U0,T):
-    return U0*np.exp(-x/T)  # b = 2*sigma**2
+def g(x,U0,T,a):
+    return U0*np.exp(-x/T)+a  # b = 2*sigma**2
+
 #p0= (1.,1.,1./100,75.)
-para, pcov = curve_fit(g,t,U,[100,250])
-U0, T = para
+para, pcov = curve_fit(g,t,U,[1,40,1.6])
+U0, T, a= para
 pcov = np.sqrt(np.diag(pcov))
-fU, fT = pcov
+fU, fT, fa= pcov
 uU = ufloat(U0, fU) 
 uT = ufloat(T, fT)
+ua = ufloat(a, fa)
 
 print('uU', uU)
 print('uT', uT)
+print('ua', ua)
 
 xx = np.linspace(0, 800, 10**4)
 
 plt.plot(t, U, 'xr', markersize=6 , label = 'Messdaten', alpha=0.5)
 plt.plot(xx, g(xx, *para), '-b', linewidth = 1, label = 'Ausgleichsfunktion', alpha=0.5)
+#plt.plot(xx, 1.70*np.exp(-xx/(3.75*10**(2))), '-y', linewidth = 1, label = 'Ausgleichsfunktion', alpha=0.5)
 plt.xlabel(r'$t \, / \, \mathrm{ns}$')
 plt.ylabel(r'$U_A \, / \, \mathrm{V}$')
 plt.legend(loc="best")                  # legend position
